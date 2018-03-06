d25a13e897052d72ae30c0f2b75600a6a213ec41
SchaellerThibault <schaeller.thibault@gmail.com>
2018-03-06 20:08:28 +0100

    UserManager + UserInterface

--- src/interfaces/UserInterface.java
+++ src/interfaces/UserInterface.java
@@ -5,52 +5,62 @@
  */
 package interfaces;
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 
 /**
  *
  * @author thibault
  */
-public interface UserInterface {
+public interface UserInterface extends Remote {
     /**
      * @return String
+     * @throws java.rmi.RemoteException
      */
-    public String getUsername();
+    public String getUsername() throws RemoteException;
     
     /**
      * @return String
+     * @throws java.rmi.RemoteException
      */
-    public String getName();
+    public String getName() throws RemoteException;
     
     /**
      * @return String
+     * @throws java.rmi.RemoteException
      */
-    public String getFirstName();
+    public String getFirstName() throws RemoteException;
     
     /**
      * @return 
+     * @throws java.rmi.RemoteException 
      */
-    public String getPassword();
+    public String getPassword() throws RemoteException;
     
     /**
      * @param username
      * @param plainPassword
      * @return boolean
+     * @throws java.rmi.RemoteException
      */
-    public boolean authenticate(String username, String plainPassword);
+    public boolean authenticate(String username, String plainPassword) throws RemoteException;
     
     /**
      * @return ArrayList
+     * @throws java.rmi.RemoteException
      */
-    public ArrayList<UserInterface> getContacts();
+    public ArrayList<UserInterface> getContacts() throws RemoteException;
     
     /**
      * @param contact 
+     * @throws java.rmi.RemoteException 
      */
-    public void addContact(UserInterface contact);
+    public void addContact(UserInterface contact) throws RemoteException;
     
     /**
      * @param contact 
+     * @throws java.rmi.RemoteException 
      */
-    public void removeContact(UserInterface contact);
+    public void removeContact(UserInterface contact) throws RemoteException;
 }
